579679220838580644456adada7431322310410f
roccotomalino <rtomalino@escuelasproa.edu.ar>
2026-04-14 12:59:38 -0300

    Create 09.py

---
+++ 09.py
@@ -0,0 +1,18 @@
+import pandas as pd
+df = pd.read_csv('StudentsPerformance.csv')
+
+print(df[['gender', 'race/ethnicity', 'parental level of education', 'lunch', 'test preparation course', 'math score', 'reading score']])
+
+print(df.head(7))
+
+print(df.shape)
+
+print(df.count())
+
+print(df.tail(2))
+
+print(df.dtypes)
+
+print(df.info())
+
+print(df.notnull())
