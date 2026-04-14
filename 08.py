9d790184daeee4af45c85bb53dd2fe2f78c03af4
roccotomalino <rtomalino@escuelasproa.edu.ar>
2026-04-14 12:59:10 -0300

    Create 08.py

---
+++ 08.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv('StudentsPerformance.csv')
+
+print(df[['gender', 'race/ethnicity', 'parental level of education','lunch', 'test preparation course',
+ 'math score', 'reading score']])
+
+print(df.head(7))
+
+print(df.shape)
+
+print(df.count())
+
+print(df.tail(2))
+
+print(df.dtypes)
